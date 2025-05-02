e863cadb29b46260192b79d93def1d648389bd14
freddyskull <freddyskull11@gmail.com>
2025-05-02 14:11:35 -0400

    mejorando el responsive

--- src/App.jsx
+++ src/App.jsx
@@ -49,12 +49,12 @@ function App() {
   }
   return (
     <div class="top-0 z-[-2] absolute bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)] bg-white w-screen h-screen">
-      <div className="mx-auto mt-6 container">
-        <img src="logo.webp" alt="LOGO UNEFM" width="250" />
+      <div className="mx-auto mt-6 ml-4 container">
+        <img src="logo.webp" alt="LOGO UNEFM" className="w-auto h-12 md:h-18" />
       </div>
-      <div className="flex flex-col justify-center items-center w-full h-[80vh]">
-        <div className="block bg-white shadow-xl p-6 py-12 border border-gray-200 rounded-lg w-[30vw]">
-          <h1 className="font-bold text-slate-600 text-2xl text-center uppercase">Buscar Estudiante</h1>
+      <div className="flex flex-col justify-center items-center px-6 w-full h-[60vh] md:h-[80vh]">
+        <div className="block bg-white shadow-xl p-6 py-12 border border-gray-200 rounded-lg w-full md:w-[30vw]">
+          <h1 className="font-bold text-slate-600 text-xl md:text-2xl text-center uppercase">Buscar Estudiante</h1>
           <div className="mt-4">
             <form className="mx-auto" onSubmit={handleBuscar}>
               <label htmlFor="default-search" className="sr-only mb-2 font-medium text-gray-900 dark:text-white text-sm">Buscar</label>
